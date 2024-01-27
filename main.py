40bf36e3f5f92a9c31888d18059ef738c3152f10
Jitesh Budhwani <jiteshkbudhwani@gmail.com>
2024-01-27 17:46:27 +0900

    Finishing step-2

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ verbose = False
 
 if '-v' in url:
     verbose=True
+    url = url[1]
+else:
+    url = url[0]
 
-protocol = url[1].split('//')[0]
+print("URL: ", url)
+protocol = url.split('//')[0]
 protocol_version = "1.1"
-rest_of_the_url = url[1].split('//')[1]
+rest_of_the_url = url.split('//')[1]
 
 host = rest_of_the_url.split('/')[0]
 method = rest_of_the_url.split('/')[1]
@@ -22,6 +26,8 @@ method = rest_of_the_url.split('/')[1]
 
 data_to_be_sent = method.upper() + " / "+protocol[0:-1].upper()+'/'+protocol_version+'\r\n'+'Host:www.'+host+'\r\n\r\n'
 
+print("data to be sent: ")
+print(data_to_be_sent.encode('utf-8'))
 
 sock = socket.socket()
 sock.connect((host, 80))
@@ -29,6 +35,7 @@ sock.connect((host, 80))
 # print('request: ')
 # print(data_to_be_sent.encode('utf-8'))
 sock.send(data_to_be_sent.encode('utf-8'))
+
 data = ''
 data = sock.recv(4096).decode()
 
@@ -36,7 +43,7 @@ headers, body = data.split('\r\n\r\n', 1)
 
 if verbose:
     print("Verbose: ")
-    print(data)
+    print(headers+body)
 else:
     print("Non verbose: ")
     print(body)
